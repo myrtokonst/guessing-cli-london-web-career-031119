cde85606e6f6bc2eb76d0102dc9a6e590d08e49e
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-10 18:05:50 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,9 +4,9 @@ puts "Guess a number between 1 and 6."
 comp_guess = rand(1..6)
 guess = gets.chomp
 while guess!=exit
- if guess == comp_guess
+ if guess.to_i == comp_guess
    puts "You guessed the correct number!"
- elsif guess!=comp_guess
+ elsif guess.to_i!=comp_guess
  puts "The computer guessed #{comp_guess}"
  elsif guess == "exit"
  break
